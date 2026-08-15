fec61e513457fa6adfb40b9e7d7e6629232b2009
Luis Romero <lerg96@gmail.com>
2026-08-15 18:55:26 -0300

    fix(plugin): use v1 plugin format {id, server} and remove class exports

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
 import { opencodeAutoMode } from "./plugin.js";
 
 export { opencodeAutoMode };
-export default opencodeAutoMode;
 
-export * from "./plugin.js";
-export * from "./config/ConfigManager.js";
-export * from "./rules/RuleEvaluator.js";
-export * from "./rules/PatternMatcher.js";
-export * from "./state/SessionState.js";
-export * from "./classifier/ClassificationService.js";
-export * from "./classifier/LLMProviderAbstraction.js";
-export * from "./classifier/TranscriptClassifier.js";
-export * from "./classifier/CircuitBreaker.js";
-export * from "./classifier/RetryHandler.js";
-export * from "./classifier/TimeoutManager.js";
-export * from "./classifier/FallbackExecutor.js";
-export * from "./deny-and-continue/DenyAndContinueService.js";
-export * from "./escalation/EscalationService.js";
-export * from "./injection/InjectionProtectionService.js";
-export * from "./permissions/PermissionPreChecker.js";
+export default {
+  id: "@lerg96/opencode-auto-mode",
+  server: opencodeAutoMode,
+};
